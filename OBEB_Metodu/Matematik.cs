244bf1a47ee6f28432eae8a647fb7b892e690c95
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 21:29:55 +0300

    Proje dosyası ekle.

---
+++ OBEB_Metodu/Matematik.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OBEB_Metodu
+{
+    class Matematik
+    {
+        public static int OBEB(int s1, int s2)
+        {
+            int s = 1;
+            int bolen = 2;
+            while (s1 != 1 && s2 != 2)
+            {
+                for (int i = 2; i < (s1 > s2 ? s1 : s2); i++)
+                {
+                    if (s1 % bolen == 0 || s2 % bolen == 0)
+                    {
+                        if (s1 % bolen == 0 && s2 % bolen == 0)
+                            s *= bolen;
+                        if (s1 % bolen == 0)
+                            s1 /= bolen;
+                        if (s2 % bolen == 0)
+                            s2 /= bolen;
+
+                    }
+                    else
+                        bolen++;
+                }
+            }
+            return s;
+        }
+    }
+}
---
+++ OBEB_Metodu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OBEB_Metodu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Convert.ToInt32(Console.ReadLine());
+            int s2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("OBEB({0},{1})= {2}", s1, s2, Matematik.OBEB(s1, s2));
+        }
+    }
+}
